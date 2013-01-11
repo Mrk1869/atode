9000381dff2d348966cced2699d349fd2aa026f1
Shoya Ishimaru <he.was.born.in.pisa.in.1564@gmail.com>
2013-01-11 20:51:16 +0900

    another method

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 (function() {
 
-  function twitter(){
-    window.twttr=window.twttr||{};
-    var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;
-    if(C>A){G=Math.round((C/2)-(A/2))}window.twttr.shareWin=window.open('http://twitter.com/share','','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
-    E=F.createElement('script');
-    E.src='http://platform.twitter.com/bookmarklets/share.js?v=1';
-    F.getElementsByTagName('head')[0].appendChild(E)
+  function twitter(title, url){
+//    window.twttr=window.twttr||{};
+//    var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;
+//    if(C>A){G=Math.round((C/2)-(A/2))}window.twttr.shareWin=window.open('http://twitter.com/share','','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
+//    E=F.createElement('script');
+//    E.src='http://platform.twitter.com/bookmarklets/share.js?v=1';
+//    F.getElementsByTagName('head')[0].appendChild(E)
+
+    if(m=prompt('','Reading')){
+      w=window.open('','_blank','width=0,height=0');
+      f=w.document.createElement('form');
+      f.setAttribute('action','http://twitter.com/statuses/update.xml');
+      f.setAttribute('method','POST');
+      i=w.document.createElement('input');
+      i.setAttribute('type','hidden');
+      i.setAttribute('name','status');
+      i.setAttribute('value',m+'%20'+title+'%20'+url);
+      f.appendChild(i);
+      w.document.documentElement.appendChild(f);
+      f.submit();
+      w.onunload=function(){
+        w.close();
+      }
+    }
+
   }
 
   function getScriptParams(){
@@ -62,6 +80,8 @@
     }
     return code;
   }
+
+  twitter();
 
   if (!document.getElementById("atode_filter")){
     var documentcode = getdocumentcode(window);
@@ -143,6 +163,6 @@
     document.charset = chr;
   }
 
-  twitter();
+  twitter(document.title, location.href);
 
 })();
