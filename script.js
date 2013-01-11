bd54b163424071c8c464ef248af179104b7367b2
Shoya Ishimaru <he.was.born.in.pisa.in.1564@gmail.com>
2013-01-11 20:07:49 +0900

    create result object

--- script.js
+++ script.js
@@ -12,6 +12,7 @@
       var element = param[i].split('=');
       var paramName = decodeURIComponent(element[0]);
       var paramValue = decodeURIComponent(element[1]);
+      result[paramName] = decodeURIComponent(paramValue);
     }
     return result;
   }
