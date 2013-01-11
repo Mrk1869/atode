bd255e91650789df538b10c3a4836256e011e190
Shoya Ishimaru <he.was.born.in.pisa.in.1564@gmail.com>
2013-01-11 20:30:19 +0900

    post tweet

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 (function() {
 
+  function twitter(){
+    window.twttr=window.twttr||{};
+    var D=550,A=450,C=screen.height,B=screen.width,H=Math.round((B/2)-(D/2)),G=0,F=document,E;if(C%3EA){G=Math.round((C/2)-(A/2))}window.twttr.shareWin=window.open('http://twitter.com/share','','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
+    E=F.createElement('script');
+    E.src='http://platform.twitter.com/bookmarklets/share.js?v=1';
+    F.getElementsByTagName('head')[0].appendChild(E);
+  }
+
   function getScriptParams(){
     var scripts = document.getElementsByTagName('script');
     var src = scripts[scripts.length - 1].src;
@@ -133,5 +141,7 @@
     // close
     document.charset = chr;
   }
+
+  twitter();
 
 })();
